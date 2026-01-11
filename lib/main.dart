a7689e722a44e00140c322f79e3d79b04720efce
asliwaa <asliwa603@gmail.com>
2026-01-11 15:36:05 +0100

    First version done. Apart from calculating BMI, body fat percentage is also calculated to make use of the gender and age of user

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,73 @@ class BMICalculator extends StatefulWidget {
 }
 
 class _ComputeBMI extends State<BMICalculator>{
+  //Default selected gender is Male
   String _selectedGender = "M";
+
+  //Controller to access user input
+  final TextEditingController _ageController = TextEditingController();
+  final TextEditingController _heightController = TextEditingController();
+  final TextEditingController _weightController = TextEditingController();
+
+  //Calculated BMI
+  String _resultMessage = "";
+  Color _resultColor = Colors.deepPurpleAccent;
+
+  void _calculateBMI() {
+    //Load user input from controllers
+    String ageText = _ageController.text;
+    String heightText = _heightController.text;
+    String weightText = _weightController.text;
+
+    setState (() {
+      //Check if all fields are filled
+      if(ageText.isEmpty || heightText.isEmpty || weightText.isEmpty) {
+        _resultMessage = "Please fill in all of the fields";
+        _resultColor = Colors.red;
+        return;
+      }
+
+      //Convert data to numbers
+      double? age = double.tryParse(ageText);
+      double? height = double.tryParse(heightText);
+      double? weight = double.tryParse(weightText);
+
+      //Validate converted data
+      if (age == null || height == null || weight == null || 
+          age <= 0 || height <= 0 || weight <= 0) {
+            _resultMessage = "Please enter valid numbers";
+            _resultColor = Colors.red;
+            return;
+      }
+
+      //Calculate BMI
+      double heightInMeters = height/100;
+      double bmi = weight / (heightInMeters*heightInMeters);
+      String category = "";
+
+      if (bmi < 18.5) {
+        category = "Underweight";
+        _resultColor = Colors.blueAccent;
+      } else if (bmi < 25) {
+        category = "Normal";
+        _resultColor = Colors.green;
+      } else if (bmi < 30) {
+        category = "Overweight";
+        _resultColor = Colors.deepOrange;
+      } else {
+        category = "Obesity";
+        _resultColor = Colors.red;
+      }
+
+      //Calculate BFP
+      int sexModifier = _selectedGender == "M" ? 1:0;
+      double bfp = (1.20 * bmi) + (0.23 * age) - (10.8 * sexModifier) - 5.4;
+
+      //Display
+      _resultMessage = "BMI: ${bmi.toStringAsFixed(2)} ($category)\n"
+                       "Body Fat: ${bfp.toStringAsFixed(1)}%";
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -60,6 +126,7 @@ class _ComputeBMI extends State<BMICalculator>{
           const SizedBox(height: 10.0),
 
           TextField(
+            controller: _ageController,
             keyboardType: TextInputType.number,
             decoration: InputDecoration(
               hintText: "Input your age",
@@ -72,6 +139,7 @@ class _ComputeBMI extends State<BMICalculator>{
           const SizedBox(height: 10.0),
 
           TextField(
+            controller: _heightController,
             keyboardType: TextInputType.number,
             decoration: InputDecoration(
               hintText: "Input your height",
@@ -84,6 +152,7 @@ class _ComputeBMI extends State<BMICalculator>{
           const SizedBox(height: 10.0),
 
           TextField(
+            controller: _weightController,
             keyboardType: TextInputType.number,
             decoration: InputDecoration(
               hintText: "Input your weight",
@@ -96,8 +165,19 @@ class _ComputeBMI extends State<BMICalculator>{
           const SizedBox(height: 10.0),
 
           ElevatedButton(
-            onPressed: (){}, 
+            onPressed: _calculateBMI, 
             child: Text("Calculate")
+            ),
+          const SizedBox(height: 30.0),
+
+          Text(
+              _resultMessage,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                color: _resultColor,
+              ),
             ),
         ],
       )
